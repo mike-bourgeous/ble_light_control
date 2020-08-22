92083965bd6e63a2ba6d07e854b26db6db992cd0
Mike Bourgeous <mike@mikebourgeous.com>
2020-08-21 19:40:58 -0600

    Allow specifying a color or white level on the command line.

--- light.sh
+++ light.sh
@@ -7,12 +7,21 @@
 # (C)2020 Mike Bourgeous, released under standard 2-clause BSD license
 
 DEVICE=$1
+ARGC=$#
 
-if [ $# != 1 ]; then
-	echo "Usage: $0 address"
+if [ $ARGC -ne 1 -a $ARGC -ne 2 -a $ARGC -ne 4 ]; then
+	echo "Usage: $0 address [w|r g b]"
 	echo "Device address must be specified in the form xx:xx:xx:xx:xx:xx"
 	exit 1
 fi
+
+# TODO: Support color effect modes and setting timers
+
+white()
+{
+	white=$1
+	printf "char-write-cmd 0x0007 56000000%02x0faa\n" $white
+}
 
 rgb()
 {
@@ -26,13 +35,34 @@ rgb()
 light_script()
 {
 	trap "echo quit; echo quit; exit" SIGINT
+
 	echo connect
-	sleep 0.75
-	for f in `seq 1 255`; do
-		rgb $f 255 $((255 - $f))
-		sleep 0.01
-	done
+	sleep 0.75 # TODO: this should wait for the connection success message
+
+	case $ARGC in
+		1)
+			echo "Color fade test" >&2
+			for f in `seq 1 255`; do
+				rgb $f 255 $((255 - $f))
+				sleep 0.01
+			done
+			;;
+
+		2)
+			echo "White light at $1" >&2
+			white $1
+			sleep 0.1
+			;;
+
+		4)
+			echo "RGB light at $1, $2, $3" >&2
+			rgb $1 $2 $3
+			sleep 0.1
+			;;
+	esac
+
 	echo quit
 }
 
-light_script | gatttool -b "$DEVICE" -I | grep -i --line-buffered -E '(fail|error|discon)'
+shift
+light_script "$@" | gatttool -b "$DEVICE" -I | grep -i --line-buffered -E '(fail|error|discon)'
